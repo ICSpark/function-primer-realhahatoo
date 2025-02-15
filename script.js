7a528331e36d77f3292d55cc1ef675fa8778d047
JeremiahS13 <148928125+realhahatoo@users.noreply.github.com>
2025-02-15 19:43:38 +0000

    Practicing Functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function initiateFunction() {
+    console.log("********************")
+    console.log("PROGRAM INITIALIZED")
+    console.log("********************")
+}
+
+function bankCalculation(dollars) {
+    console.log("$", dollars + " is worth", dollars * 4, (" quarters"))
+}
+initiateFunction()
+bankCalculation(5)
+bankCalculation(10)
+
+initiateFunction()
+bankCalculation(2)
+
+initiateFunction()
+bankCalculation(0)
